eda4d34fc1dce2d811fdb53af5fd265ba1ade014
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-29 13:21:30 +0000

    Deploy open-power/op-build to github.com/open-power/op-build.git:gh-pages

---
+++ _static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'v2.3-rc2-138-g66f83ba0',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
